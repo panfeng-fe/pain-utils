4d5775395656f3773d003c5051768bb33ecf7aba
feng.pan <feng.pan@trip.com>
2022-08-08 00:17:23 +0800

    fix: add err func

--- perr/error.go
+++ perr/error.go
@@ -1,4 +1,14 @@
+/*
+ * @Author: feng.pan@trip.com
+ * @Date: 2022-03-27 01:40:00
+ * @LastEditors: feng.pan
+ * @LastEditTime: 2022-08-08 00:16:52
+ * @FilePath: /pain-utils/perr/error.go
+ * @Description: todo...
+ */
 package perr
+
+import "fmt"
 
 func PanicErrDouble[T any](res T, err error) T {
 	if err != nil {
@@ -10,5 +20,11 @@ func PanicErrDouble[T any](res T, err error) T {
 func PanicErr(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func PrintErr(err error) {
+	if err != nil {
+		fmt.Printf("err: %s", err)
 	}
 }
